130aad206cd5078143d2da293499caa8e5ff8b88
Sarah D. Perea <sdperea.1@gmail.com>
2019-01-02 15:52:41 +0800

    Added some documentations

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+/*
+onInstalled is fired when extension is first installed,
+extension is updated to a new version,
+and when Chrome is updated to a new version.
+*/
+
+/*
+chrome.storage API is used to store, retrieve and track changes to user 
+data.
+related: localStorage API
+*/
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log("The color is green.");
